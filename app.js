5e052c622b99f25f55e5b389e4fd578c3d872112
KELowerBrent <suman.kumari@uwl.ac.uk>
2026-07-07 03:20:03 +0100

    Refactor map initialization and add base layers

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 
 const map = L.map("map").setView([51.7, -0.8], 15);
 
+L.control.scale({ position: 'bottomright', metric:true, imperial:false, maxWidth: 200 }).addTo(map);
+
 // Base Layer
 
 L.tileLayer(
@@ -9,7 +11,48 @@ L.tileLayer(
 {
     maxZoom:19,
     attribution:"© OpenStreetMap"
+});
+
+var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    maxZoom: 17,
+    attribution: '&copy; OpenTopoMap contributors'
+});
+
+var cartoLight = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; <a href="https://carto.com">CARTO</a>'
+});
+
+var satellite = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    attribution: '&copy; Esri'
+})
+//.addTo(map); // Set default active base layer
+
+var dark = L.tileLayer(
+'https://{s}.basemaps.cartocdn.com/rastertiles/dark_all/{z}/{x}/{y}{r}.png',
+{
+    attribution: '&copy; CARTO'
 }).addTo(map);
+
+var baseMaps = {
+    "OpenStreetMap": osm,
+    "OpenTopoMap": topo,
+    "CartoDB Light": cartoLight,
+    "Esri Satellite": satellite
+};
+
+// const geoJsonFiles = [
+//     { path: './Data/London_Ward_json.geojson', color: '#3388ff', label: 'Ward' },
+//     { path: './Data/London_Boroughs.geojson', color: '#ff5533', label: 'Borough' },
+//     { path: './Data/Lower_brent_river.geojson', color: '#2ecc71', label: 'Lower Brent River' },
+//     { path: './Data/Lower_brent_river_catchment.geojson', color: '#9b59b6', label: 'Catchment' }
+// ];
+
+
+var overlayMaps = {}
+    
+    
+L.control.layers(baseMaps, overlayMaps, { collapsed: true, position:'bottomleft'}).addTo(map);
+
 
 let rasterLayer;
 let georaster;
