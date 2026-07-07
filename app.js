3775ea251dc88d7bfdca8d16474f8a558ebf72f9
KELowerBrent <suman.kumari@uwl.ac.uk>
2026-07-07 03:40:47 +0100

    Add support for 2026 GeoTIFF and update click handling
    Updated raster loading functionality to include a second GeoTIFF for 2026. Adjusted click event handling to display values from the new raster.

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ L.control.layers(baseMaps, { collapsed: true, position:'bottomleft'}).addTo(map)
 let rasterLayer;
 let georaster;
 
-// Load GeoTIFF from GitHub repository
+// Load GeoTIFF from GitHub repository-2017
 
 fetch("Data/Image_Landsat_2017_LST_catchment.tif")
 
@@ -83,17 +83,88 @@ fetch("Data/Image_Landsat_2017_LST_catchment.tif")
 
 map.on("click",function(e){
 
-    document.getElementById("lat").innerHTML=e.latlng.lat.toFixed(6);
+    document.getElementById("lat").innerHTML=e.latlng.lat.toFixed(3);
 
-    document.getElementById("lng").innerHTML=e.latlng.lng.toFixed(6);
+    document.getElementById("lng").innerHTML=e.latlng.lng.toFixed(3);
 
     if(!georaster) return;
 
     const value=getRasterValue(e.latlng);
 
-    document.getElementById("value").innerHTML=value;
+    document.getElementById("value_1").innerHTML=value;
 
 });
+
+
+// Read Raster Value
+
+function getRasterValue(latlng){
+
+    const xmin=georaster.xmin;
+    const ymax=georaster.ymax;
+
+    const pixelWidth=georaster.pixelWidth;
+    const pixelHeight=georaster.pixelHeight;
+
+    const x=Math.floor((latlng.lng-xmin)/pixelWidth);
+
+    const y=Math.floor((ymax-latlng.lat)/pixelHeight);
+
+    if(
+        x<0 ||
+        y<0 ||
+        x>=georaster.width ||
+        y>=georaster.height
+    ){
+
+        return "Outside Raster";
+
+    }
+
+    const value=georaster.values[0][y][x];
+
+    return value;
+
+    // Load GeoTIFF from GitHub repository-2026
+
+fetch("Data/Image_Landsat_2026_LST_catchment_wgs84.tif")
+
+.then(response=>response.arrayBuffer())
+
+.then(arrayBuffer=>parseGeoraster(arrayBuffer))
+
+.then(raster=>{
+
+    georaster=raster;
+
+    rasterLayer=new GeoRasterLayer({
+
+        georaster:georaster,
+        opacity:0.7
+
+    });
+
+    rasterLayer.addTo(map);
+
+    map.fitBounds(rasterLayer.getBounds());
+
+});
+
+// // Mouse Click
+
+// map.on("click",function(e){
+
+//     document.getElementById("lat").innerHTML=e.latlng.lat.toFixed(6);
+
+//     document.getElementById("lng").innerHTML=e.latlng.lng.toFixed(6);
+
+//     if(!georaster) return;
+
+//     const value=getRasterValue(e.latlng);
+
+//     document.getElementById("value_2").innerHTML=value;
+
+// });
 
 
 // Read Raster Value
